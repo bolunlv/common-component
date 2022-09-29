8de1c1161d5c0aefd6bfafdea6bb24e7ae788018
linjialun <linjialun@bonree.com>
2022-09-29 12:59:10 +0800

    feat(组建同步)：同步所有以开发组件 （git 仓库创建之后完善脚手架）

---
+++ component/EoiPlus/index.ts
@@ -0,0 +1,38 @@
+import {
+  EoiBreadcrumb,
+  EoiEditor,
+  EoiFileView,
+  EOIHeader,
+  EoiHeaderSearch,
+  EoiIframeView,
+  EoiLastingSearch,
+  EOILoginControl,
+  EOINavControl,
+  EoiRouterTabs,
+  EOISidNavigation,
+  EoiSvgIcon,
+  EoiTableHeader,
+  EoiTableView,
+  EoiTableViewV2,
+  EOIUserControl,
+} from '@eoi-plus/components';
+import { withAllInstall } from '@eoi-plus/utils';
+
+export default withAllInstall([
+  EOIHeader,
+  EOIUserControl,
+  EOISidNavigation,
+  EOINavControl,
+  EOILoginControl,
+  EoiHeaderSearch,
+  EoiRouterTabs,
+  EoiTableHeader,
+  EoiTableView,
+  EoiTableViewV2,
+  EoiFileView,
+  EoiEditor,
+  EoiBreadcrumb,
+  EoiLastingSearch,
+  EoiSvgIcon,
+  EoiIframeView,
+]);
